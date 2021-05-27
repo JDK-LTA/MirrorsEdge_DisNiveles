4d3be1fe37d1b67453ddc3903ced4dbb824e32d6
Jota <jotagamedev@gmail.com>
2021-05-27 18:25:18 +0200

    deadzone2.0

--- Mirror's_Prototype/Assets/Scripts/DeadZone.cs
+++ Mirror's_Prototype/Assets/Scripts/DeadZone.cs
@@ -2,8 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider))]
 public class DeadZone : MonoBehaviour
 {
+    private void Start()
+    {
+        Rigidbody rb;
+        rb = GetComponent<Rigidbody>();
+        if (!rb)
+        {
+            rb = gameObject.AddComponent<Rigidbody>();
+        }
+        rb.useGravity = false;
+        rb.isKinematic = true;
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+        GetComponent<BoxCollider>().isTrigger = true;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<MARKER>())
